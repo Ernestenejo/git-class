c84edc7f203a1cc7b1591212d85267222766d1dd
Billazeezdinho <billazeezdinho@gmail.com>
2025-02-21 09:43:32 -0600

    dinho

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get("/contact", (req, res)=>{
     res.send("contact me on larryamadi223@gmail.com")
 });
 app.get("/About", (req, res)=>{
-    res.send("check About me on     h billazeezdinho on all social media")
+    res.send("check About me on billazeezdinho on all social media")
 });
 app.listen(port,()=>{
     console.log("we are live on port " +port)
